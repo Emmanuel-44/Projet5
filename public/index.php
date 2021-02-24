4c9605657a64c63ab3cb297cc58e19a7e2874123
Emmanuel-44 <emmanuel44.mahe@gmail.com>
2021-02-24 16:26:28 +0100

    fixes #5, fixes #6, Routes configuration and template integration using twig

--- public/index.php
+++ public/index.php
@@ -1,11 +1,58 @@
 <?php
 require '../vendor/autoload.php';
 
+use controllers\PostController;
+use controllers\UserController;
+
 $router = new AltoRouter();
 $router->setBasePath('/projet5');
 
+// map homepage
 $router->map('GET', '/', function() {
-    echo "accueil";
+    $postController = new PostController();
+    $postController->index();
+});
+
+// map Blog
+$router->map('GET', '/blog', function() {
+    $postController = new PostController();
+    $postController->blog();
+});
+
+// map single Post
+$router->map('GET', '/blog/[*:slug]-[i:id]', function() {
+    $postController = new PostController();
+    $postController->single();
+});
+
+// map login
+$router->map('GET', '/login', function() {
+    $userController = new UserController();
+    $userController->login();
+});
+
+// map Home admin
+$router->map('GET', '/admin', function() {
+    $postController = new PostController();
+    $postController->adminIndex();
+});
+
+// map create Post
+$router->map('GET', '/admin/ajouter', function() {
+    $postController = new PostController();
+    $postController->create();
+});
+
+// map read Post
+$router->map('GET', '/admin/article/[*:slug]-[i:id]', function() {
+    $postController = new PostController();
+    $postController->read();
+});
+
+// map update Post
+$router->map('GET', '/admin/modifier/[*:slug]-[i:id]', function() {
+    $postController = new PostController();
+    $postController->update();
 });
 
 // match curent request url
---
+++ src/controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+namespace controllers;
+
+class PostController
+{
+    public function index()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('frontend/homeView.twig');
+    }
+
+    public function blog()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('frontend/blogView.twig');
+    }
+
+    public function single()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('frontend/singleView.twig');
+    }
+
+    public function adminIndex()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('backend/homeView.twig');
+    }
+
+    public function create()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('backend/createView.twig');
+    }
+
+    public function read()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('backend/readView.twig');
+    }
+
+    public function update()
+    {
+        $twig = TwigFactory::twig();
+        echo $twig->render('backend/updateView.twig');
+    }
+}
